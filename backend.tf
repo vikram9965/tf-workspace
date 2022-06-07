8183841d41b9a1e42e6853faa28f459c9b3c34fc
vikram9965 <103040945+vikram9965@users.noreply.github.com>
2022-06-07 16:00:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-terraform-state-ap-south-1"
-    key            = "tfdemo/workspace"
+    bucket         = "ctl-terraform-state01"
+    key            = "dev"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state"
+    dynamodb_table = "tf-state-lock"
   }
 }
