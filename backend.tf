57e29cc8aa4d62310c89502adc90a5cf2ba04635
vikram9965 <103040945+vikram9965@users.noreply.github.com>
2022-06-07 16:38:59 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ctl-terraform-state01"
-    key            = "dev"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-state-lock"
-  }
-}
